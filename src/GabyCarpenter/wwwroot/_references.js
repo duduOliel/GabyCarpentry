0385214539593274da0248f754d1e87f689501ae
Dudu Oliel <oliel@yahoo.com>
2016-10-30 00:31:27 +0300

    added logo

--- src/GabyCarpenter/wwwroot/_references.js
+++ src/GabyCarpenter/wwwroot/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/image.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.js" />
